c12fd26b23fbb50a72f748cb27f2652c505246ba
Jeff Passetti <jpassetti@gmail.com>
2022-01-13 14:58:30 -0500

    updated homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,91 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import ButtonUI from '../components/ButtonUI'
+import ButtonGroup from '../components/ButtonGroup'
+import Container from '../components/Container'
+import TitleFeed from '../components/TitleFeed'
+import Frame from '../components/Frame'
+import { useState, useEffect } from 'react'
 
-const Homepage = () => {
-  return <div>
-	<h1>Newhouse 1 lobby</h1>
-	<Link href="/lobby">
-		<a>
-			Preview
-		</a>
-	</Link>
+import { getPosters } from '../lib/posters'
+
+import Row from '../components/Row'
+import TV from '../components/TV'
+import Zoom from '../components/Zoom'
+
+const Lobby = () => {
+	const posters = getPosters()
+	const [zoomLevel, setZoomLevel] = useState(.5)
+	const [currentPosterIndex, setCurrentPosterIndex] = useState(0)
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			if (currentPosterIndex >= posters.length - 1) {
+				setCurrentPosterIndex(0);
+			} else {
+				setCurrentPosterIndex(currentPosterIndex + 1);
+			}
+		}, 15000);
+		// clearing interval
+		return () => clearInterval(timer);
+	});
+
+	return <div style={
+		{
+			width: "3000px",
+			transition: `all .5s`,
+			transformOrigin: "top left",
+			transform: `scale(${zoomLevel})`
+		}
+	}>
+		<ButtonGroup>
+			Zoom:
+			<ButtonUI label="50%" clickHandler={() => {
+				setZoomLevel(0.5)
+			}} />
+			<ButtonUI label="100%" clickHandler={() => {
+				setZoomLevel(1)
+			}} />
+			<ButtonUI label="Previous" clickHandler={() => {
+				if (currentPosterIndex <= 0) {
+					setCurrentPosterIndex(posters.length - 1);
+				} else {
+					setCurrentPosterIndex(currentPosterIndex - 1);
+				}
+			}} />
+			<ButtonUI label="Next" clickHandler={() => {
+				if (currentPosterIndex >= posters.length - 1) {
+					setCurrentPosterIndex(0);
+				} else {
+					setCurrentPosterIndex(currentPosterIndex + 1);
+				}
+			}} />
+		</ButtonGroup>
+		<Row justifyContent="space-evenly" alignItems="center" mt="2">
+			<TV size="32x9">
+				<Zoom size="3.5">
+					<Frame titleFeed src={`/feed/title/${posters[currentPosterIndex].slug}`} />
+				</Zoom>
+			</TV>
+			<TV size="16x9">
+				<Zoom size="3.1">
+					<Frame contentFeed src={`/feed/content/${posters[currentPosterIndex].slug}`} />
+				</Zoom>
+			</TV>
+			<TV size="32x9">
+				<Zoom size="3.5">
+					<Frame titleFeed src={`/feed/title/${posters[currentPosterIndex].slug}`} />
+				</Zoom>
+			</TV>
+			<TV size="16x9">
+				<Zoom size="3.1">
+					<Frame photoFeed src={`/feed/photo/${posters[currentPosterIndex].slug}`} />
+				</Zoom>
+			</TV>
+			<TV size="32x9">
+				<Zoom size="3.5">
+					<Frame titleFeed src={`/feed/title/${posters[currentPosterIndex].slug}`} />
+				</Zoom>
+			</TV>
+		</Row>
 	</div>
 }
-export default Homepage
+export default Lobby
